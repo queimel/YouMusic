e4013d44f2e2715fc25e3f542461485ae7f60c6d
Cristian Campos <cristian.campos@globant.com>
2018-03-29 11:43:44 -0300

    repara config inicial

--- src/hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Hello extends React.Component {
-  render() {
-    return React.DOM.h1({ className: "mystyle" }, `Hello ${this.props.name}!`);
-  }
-}
-
-export default Hello;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,4 @@
 import '../src/scss/styles.scss';
-import React from 'react';
-
-import Hello from './hello';
 //var _ = require('lodash');
 
 
@@ -11,12 +8,10 @@ import Hello from './hello';
 
 //console.log('ffdhsi');
 
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
-);*/
-
-ReactDOM.render(<Hello name='React' />, document.getElementById('root'));
+);
